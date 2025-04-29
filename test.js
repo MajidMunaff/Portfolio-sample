cedce18a96287216e46ecb63a5bd7c9fa5d9548a
MajidMunaff <majidbeyond@gmail.com>
2025-04-29 12:32:02 +0530

    done

---
+++ test.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// const cVowels = s => {
+//   const vowels = 'aeiouAEIOU';
+//   let count = 0;
+
+//   for (const char of s) {
+//     if (vowels.includes(char)) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// };
+
+// const s = 'Hello World';
+// console.log(cVowels(s));
+
+// let str = 'majid';
+// for (let n in str) {
+//   console.log(str[n]);
+// }
+
+// let str = 'hello guys my name is Drift';
+// let vowels = 'aeiouAEIOU';
+// let count = 0;
+// for (let n in str) {
+//   if (vowels.includes(str[n])) {
+//     count++;
+//     // console.log(count);
+//   }
+// }
+// console.log(count);
+
+// let str = [10, -10, 10, -9, 2, 0, 4, 5, 6, 120, 239];
+// let newString = [];
+// for (let char in str) {
+//   if (str[char] > 0) newString.push(str[char]);
+// }
+// // newString = newString.join('');
+// let sroted = newString.sort(function (a, b) {
+//   return b - a;
+// });
+// console.log(sroted);
+
+// let str = 'hello guys g  f';
+// let newStr = str.split('');
+// console.log(newStr);
